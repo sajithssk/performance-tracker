3633d718a55abd99e9d5e1b5d417e1d2f93f48d4
SajithSK <sajithsajan@gmail.com>
2026-06-04 00:44:13 +0530

    initial commit for employee performance tracker API

---
+++ src/main/java/com/hr/performancetracker/PerformanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.hr.performancetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceTrackerApplication.class, args);
+    }
+
+}
